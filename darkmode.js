a2f12b93b380b5b6a4561345e75b0efb1f59b9f7
Bernardpxl <daviantonino07@gmail.com>
2025-11-08 11:29:07 -0300

    Refactor dark mode functionality
    Removed redundant code and preserved toggleMode function.

--- darkmode.js
+++ darkmode.js
@@ -5,23 +5,3 @@ function toggleMode() {
     switchBall.textContent = isDark ? "🌙" : "🌞";
 
   }
-
-
-    const modal = document.getElementById('modal');
-    const fecharBtn = document.querySelector('.fechar');
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach(card => {
-      card.addEventListener('click', () => {
-        document.querySelector('.modal-content h2').innerText = card.innerText;
-        modal.style.display = 'flex';
-      });
-    });
-
-    fecharBtn.addEventListener('click', () => {
-      modal.style.display = 'none';
-    });
-
-    window.addEventListener('click', (e) => {
-      if (e.target === modal) modal.style.display = 'none';
-    });
