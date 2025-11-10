57bbaa928b1ae7024ddf09cbd1f874a0e8c2890b
Bernardpxl <daviantonino07@gmail.com>
2025-11-09 22:54:37 -0300

    Enhance dark mode with modal event listeners
    Added event listeners for modal functionality and close actions.

--- darkmode.js
+++ darkmode.js
@@ -5,3 +5,26 @@ function toggleMode() {
     switchBall.textContent = isDark ? "🌙" : "🌞";
 
   }
+
+ document.getElementById("btnWeb").addEventListener("click", () => {
+  document.getElementById("modalWeb").style.display = "block";
+});
+
+document.getElementById("btnDesign").addEventListener("click", () => {
+  document.getElementById("modalDesign").style.display = "block";
+});
+
+// Fecha o modal ao clicar no X
+document.querySelectorAll(".close").forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    const modalId = e.target.getAttribute("data-close");
+    document.getElementById(modalId).style.display = "none";
+  });
+});
+
+// Fecha o modal ao clicar fora dele
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    e.target.style.display = "none";
+  }
+});
