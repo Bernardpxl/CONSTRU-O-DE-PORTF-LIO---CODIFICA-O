d5b596c2614b50520d960030defb3a0fa85edc41
Bernardpxl <daviantonino07@gmail.com>
2025-11-12 19:38:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ function toggleMode() {
     const isDark = document.body.classList.contains("dark-mode");
     const switchBall = document.getElementById("switchBall");
     switchBall.textContent = isDark ? "🌙" : "🌞";
+
   }
+
+ document.getElementById("btnWeb").addEventListener("click", () => {
+  document.getElementById("modalWeb").style.display = "block";
+});
+
+document.getElementById("btnDesign").addEventListener("click", () => {
+  document.getElementById("modalDesign").style.display = "block";
+});
+
+
+document.querySelectorAll(".close").forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    const modalId = e.target.getAttribute("data-close");
+    document.getElementById(modalId).style.display = "none";
+  });
+});
+
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal")) {
+    e.target.style.display = "none";
+  }
+});
